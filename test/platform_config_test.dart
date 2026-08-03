49a5fea64bff1e977627a3879ffe43c6d23e5172
darkspike1988 <impedire@googlemail.com>
2026-08-03 03:18:52 +0200

    test Android variant manifest placeholders

--- test/platform_config_test.dart
+++ test/platform_config_test.dart
@@ -3,16 +3,30 @@ import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('Android registers the wachbuch connect URI scheme', () async {
+  test('Android registers isolated Wachbuch connect URI schemes', () async {
     final manifest = await File(
       'android/app/src/main/AndroidManifest.xml',
     ).readAsString();
+    final gradle = await File('android/app/build.gradle.kts').readAsString();
 
     expect(manifest, contains('android.intent.action.VIEW'));
     expect(manifest, contains('android.intent.category.BROWSABLE'));
-    expect(manifest, contains('android:scheme="wachbuch"'));
+    expect(manifest, contains('android:scheme="\${appScheme}"'));
     expect(manifest, contains('android:host="connect"'));
     expect(manifest, contains('android.permission.ACCESS_COARSE_LOCATION'));
+    expect(manifest, contains('android:allowBackup="false"'));
+    expect(manifest, contains('android:dataExtractionRules="@xml/data_extraction_rules"'));
+
+    expect(
+      gradle,
+      contains('manifestPlaceholders["appScheme"] = "wachbuch"'),
+    );
+    expect(
+      gradle,
+      contains('manifestPlaceholders["appScheme"] = "wachbuch-internal"'),
+    );
+    expect(gradle, contains('applicationIdSuffix = ".internal"'));
+    expect(gradle, contains('manifestPlaceholders["appLabel"] = "Wachbuch Internal"'));
   });
 
   test('iOS registers the wachbuch URI scheme', () async {
