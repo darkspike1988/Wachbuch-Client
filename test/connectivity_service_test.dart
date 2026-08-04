e9cbf497274cfda87a3e2788cf444df7daafda91
Michael Katschko <Michael.Katschko@icloud.com>
2026-08-04 20:27:50 +0200

    fix: remove unused foundation import

--- test/connectivity_service_test.dart
+++ test/connectivity_service_test.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:wachbuch_mobile/services/connectivity_service.dart';
 
