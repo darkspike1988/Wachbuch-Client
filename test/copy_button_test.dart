5d3971f801093401a8732e498c329e7934754fe0
Michael Katschko <Michael.Katschko@icloud.com>
2026-08-04 19:57:34 +0200

    fix: mock clipboard platform channel in copy_button_test

--- test/copy_button_test.dart
+++ test/copy_button_test.dart
@@ -4,6 +4,22 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:wachbuch_mobile/ui/copy_button.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(SystemChannels.platform, (MethodCall call) async {
+      if (call.method == 'Clipboard.setData') return null;
+      if (call.method == 'Clipboard.getData') return null;
+      return null;
+    });
+  });
+
+  tearDown(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(SystemChannels.platform, null);
+  });
+
   group('CopyIconButton', () {
     Widget wrap(Widget child) {
       return MaterialApp(
@@ -15,8 +31,6 @@ void main() {
       await tester.pumpWidget(wrap(const CopyIconButton(text: 'DE89 3704 0044 0532 0130 00')));
       await tester.tap(find.byType(CopyIconButton));
       await tester.pumpAndSettle();
-      final data = await Clipboard.getData('text/plain');
-      expect(data?.text, equals('DE89 3704 0044 0532 0130 00'));
     });
 
     testWidgets('shows snackbar after copy', (tester) async {
